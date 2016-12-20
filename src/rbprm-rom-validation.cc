d7f238107fd61a8972cd221b009a73e48ccbe204
Mylene Campana <mylene.campana@laas.fr>
2016-12-20 14:30:57 +0100

    Fix big with ROM collision-checking (introduced by the branch merge)

--- src/rbprm-rom-validation.cc
+++ src/rbprm-rom-validation.cc
@@ -32,37 +32,37 @@ namespace hpp {
     }
 
     RbPrmRomValidation::RbPrmRomValidation (const model::DevicePtr_t& robot
-                                           ,const std::vector<std::string>& affFilters)
-        : hpp::core::CollisionValidation(robot)
-        , filter_(affFilters)
-        , unusedReport_(new CollisionValidationReport) {}
+					    ,const std::vector<std::string>& affFilters)
+      : hpp::core::CollisionValidation(robot)
+      , filter_(affFilters)
+      , unusedReport_(new CollisionValidationReport) {}
 
     bool RbPrmRomValidation::validate (const Configuration_t& config)
     {
-        return validate(config, unusedReport_);
+      return validate(config, unusedReport_);
     }
 
     bool RbPrmRomValidation::validate (const Configuration_t& config,
-                    ValidationReportPtr_t& validationReport)
+				       ValidationReportPtr_t& validationReport)
     {
       ValidationReportPtr_t romReport;
       bool collision = !hpp::core::CollisionValidation::validate(config, romReport);
       CollisionValidationReportPtr_t reportCast = boost::dynamic_pointer_cast<CollisionValidationReport>(romReport);
-      //hppDout(notice,"number of contacts  : "<<reportCast->result.numContacts());
-      //hppDout(notice,"contact 1 "<<reportCast->result.getContact(0).pos);
       RbprmValidationReportPtr_t rbprmReport =boost::dynamic_pointer_cast<RbprmValidationReport>(validationReport);
-      if(rbprmReport){
-        //hppDout(notice,"rbprm-validation-report correctly cast");
-      }else{
-        hppDout(notice,"Validation report is not a valid rbprm-validation-report instance");
-      }
-      if(rbprmReport){  // if the report is a correct rbprm report, we add the rom information
-        rbprmReport->ROMReports.insert(std::make_pair(robot_->name(),boost::dynamic_pointer_cast<CollisionValidationReport>(romReport)));
-        rbprmReport->ROMFilters.insert(std::make_pair(robot_->name(),collision));
-      }else{
-        validationReport = romReport;
-    }
-return collision;
+      if(!rbprmReport)
+        hppDout(notice,"problem cast rbprm-validation-report instance");
+
+      if(collision)
+        {
+	  if(rbprmReport){  // if the report is a correct rbprm report, we add the rom information
+	    rbprmReport->ROMReports.insert(std::make_pair(robot_->name(),boost::dynamic_pointer_cast<CollisionValidationReport>(romReport)));
+	    rbprmReport->ROMFilters.insert(std::make_pair(robot_->name(),collision));
+	  }else{
+	    validationReport = romReport;
+	  }
+	}
+      rbprmReport->ROMFilters.insert(std::make_pair(robot_->name(),collision)); // here report = 0;;
+      return collision;
     }
 
   }// namespace rbprm
